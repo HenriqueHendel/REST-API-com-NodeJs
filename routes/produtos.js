a79776ad0d550d72ccbde7a5ef60b50480e7ae07
Henrique Hendel <henrique.henriquelopess@gmail.com>
2020-06-03 01:44:21 -0300

    Alterando tabela de produtos com a Api

--- routes/produtos.js
+++ routes/produtos.js
@@ -4,43 +4,102 @@ const mysql = require("../mysql").pool;
 
 // Rota para listar produtos
 router.get("/",(req,res,next)=>{
-    res.status(200).send({
-        message:"Usando o GET dentro da rota de produtos"
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error})};
+        conn.query("SELECT * from produtos",null,(error,resultado,fields)=>{
+            conn.release();
+            if(error){
+                return res.status(500).send({
+                    error:error
+                })
+            }
+            res.status(200).send({response:resultado});
+        })
     })
 });
 
 // Rota para adicionar produtos
 router.post("/", (req,res,next)=>{
-    const produto = {
-        nome: req.body.nome,
-        preco: req.body.preco
-    }
-    
-    res.status(201).send({
-        message: "Usando o POST dentro da rota de produtos",
-        pedidoCriado: produto
+    mysql.getConnection((erro,conn)=>{
+        if(erro){return res.status(500).send({error:erro})};
+        conn.query("INSERT INTO produtos (nome, preco) values (?,?)",
+        [req.body.nome, req.body.preco],
+        (error,resultado,field)=>{
+            conn.release();
+            if(error){
+                return res.status(500).send({
+                    error:error,
+                    response:null
+                });
+            }
+            res.status(201).send({
+                message:"Produto inserido com sucesso",
+                id_produto: resultado
+            });
+
+        })
     })
 })
 
 // Rota para listar produtos específicos
 router.get("/:id",(req,res,next)=>{
-    res.status(200).send({
-        message:"Usando o GET em um produto específico",
-        id:req.params.id
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({error:error})};
+        conn.query("SELECT * from produtos where id_produto=?",[req.params.id],(error,resultado,fields)=>{
+            conn.release();
+            if(error){
+                return res.status(500).send({
+                    error:error
+                })
+            }
+            res.status(200).send({response:resultado});
+        })
     })
 })
 
 // Rota para alterar algo de um produto
 router.patch("/",(req,res,next)=>{
-    res.status(201).send({
-        message:"Usando o PATCH dentro da rota de produtos"
+    mysql.getConnection((erro,conn)=>{
+        if(erro){return res.status(500).send({error:erro})};
+        conn.query("UPDATE produtos set nome = ?, preco=? where id_produto=?",
+        [req.body.nome, req.body.preco, req.body.id_produto],
+        (error,resultado,field)=>{
+            conn.release();
+            if(error){
+                return res.status(500).send({
+                    error:error,
+                    response:null
+                });
+            }
+            res.status(202).send({
+                message:"Produto alterado com sucesso",
+                id_produto: resultado
+            });
+
+        })
     })
 })
 
 // Rota para deletar um produto
 router.delete("/",(req,res,next)=>{
-    res.status(201).send({
-        message:"Usando o DELETE dentro da rota de produtos"
+    mysql.getConnection((erro,conn)=>{
+        if(erro){return res.status(500).send({error:erro})};
+        conn.query("delete from produtos where id_produto = ?",
+        [ req.body.id_produto],
+        (error,resultado,field)=>{
+            conn.release();
+            if(error){
+                return res.status(500).send({
+                    error:error,
+                    response:null
+                });
+            }
+            res.status(202).send({
+                message:"Produto deletado com sucesso",
+                id_produto: resultado
+            });
+
+        })
     })
 })
 
