6895c46fc4af94570acb780bea04a20f6646c75a
Henrique Hendel <henrique.henriquelopess@gmail.com>
2020-06-23 22:51:27 -0300

    Rota de Login e criação de Token com JWT

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("../mysql").pool;
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.post("/cadastro", (req, res, next)=>{
     mysql.getConnection((erro,conn)=>{
@@ -30,5 +31,36 @@ router.post("/cadastro", (req, res, next)=>{
         });
     });
 });
+
+router.post("/login",(req,res,next)=>{
+    mysql.getConnection((erro,conn)=>{
+        if(erro){return res.status(500).send({Erro: erro})}
+        conn.query("SELECT * FROM usuarios WHERE email=?",[req.body.email],(error,result,fields)=>{
+            conn.release();
+            if(error){return res.status(500).send({Erro: error})};
+            if(result.length<1){
+                return res.status(401).send({message: "Falha na autenticação"});
+            }
+            bcrypt.compare(req.body.senha,result[0].senha,(erro,response)=>{
+                if(erro){return res.status(401).send({message:"Falha na autenticação"})};
+                if(response){
+                    const token = jwt.sign({
+                        id_usuario:result[0].id_usuario,
+                        email: result[0].email
+                        },
+                        process.env.JWT_KEY || "segredo",
+                        {
+                            expiresIn:"1h"
+                        }
+                    );
+
+                    return res.status(200).send({message:"Autenticado com Sucesso",token:token});
+                };
+                return res.status(401).send({message:"Falha na autenticação"});
+
+            })
+        })
+    })
+})
 
 module.exports = router;
